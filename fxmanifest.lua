6a5e001f8eef13d6d52def493f7e8352d8bfcad7
Hipera <154025658+Hipera@users.noreply.github.com>
2024-11-06 15:16:16 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+shared_script '@es_extended/imports.lua'
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Hipera'
+description 'Teleport Script using ox_target'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'es_extended'
+}
