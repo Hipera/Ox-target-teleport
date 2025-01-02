1c8f9f1a02f023cb8501040d07bea1ac8d07050f
Hipera <154025658+Hipera@users.noreply.github.com>
2025-01-02 12:59:19 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,8 +1,5 @@
--- YOU CAN ADD MULTIPLE TELEPORTS
-
 ESX = exports["es_extended"]:getSharedObject()
 
--- Get ESX instance
 Citizen.CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
@@ -10,24 +7,23 @@ Citizen.CreateThread(function()
     end
 end)
 
--- Function to teleport the player
 local function teleportPlayer(coords)
     local playerPed = PlayerPedId()
     SetEntityCoords(playerPed, coords.x, coords.y, coords.z, false, false, false, true)
     TriggerEvent('esx:showNotification', 'You have been teleported!')
 end
 
--- Targeting setup for teleportation
+
 exports['ox_target']:addSphereZone({
-    coords = vector3(200.0, -1000.0, 30.0), -- Location where the target will be
+    coords = vector3(200.0, -1000.0, 30.0),
     radius = 1.5,
     options = {
         {
             name = 'teleport',
             label = 'Teleport to Location',
-            icon = 'map', -- Changed to a map icon
+            icon = 'map',
             onSelect = function()
-                local teleportCoords = vector3(500.0, -500.0, 30.0) -- location that you will teleport to
+                local teleportCoords = vector3(500.0, -500.0, 30.0)
                 teleportPlayer(teleportCoords)
             end
         }
@@ -35,15 +31,15 @@ exports['ox_target']:addSphereZone({
 })
 
 exports['ox_target']:addSphereZone({
-    coords = vector3(195.6024, -997.7037, 30.0919), -- Location where the target will be
+    coords = vector3(195.6024, -997.7037, 30.0919),
     radius = 1.5,
     options = {
         {
             name = 'teleport',
             label = 'Teleport to Location',
-            icon = 'map', -- Changed to a map icon
+            icon = 'map',
             onSelect = function()
-                local teleportCoords = vector3(500.0, -500.0, 30.0) -- location that you will teleport to
+                local teleportCoords = vector3(500.0, -500.0, 30.0)
                 teleportPlayer(teleportCoords)
             end
         }
